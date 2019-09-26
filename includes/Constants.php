0a53f30d1d7ceed500391d7bb76961558c4981ba
StunIsLove <54876681+StunIsLove@users.noreply.github.com>
2019-09-27 00:06:37 +0600

    Create Constants.php

---
+++ includes/Constants.php
@@ -0,0 +1,11 @@
+<?php
+
+// File       : Constants.php
+// Description: Constants
+// Last edit  : 30.01.2018 [Stanislav Koshevoy] - комментирование кода
+
+	//Задаём постоянные
+	define('DB_NAME','name'); //Указать имя базы данных.
+	define('DB_USER','login'); //Указать имя пользователя БД.
+	define('DB_PASS','pass'); //Указать пароль от БД.
+	define('DB_HOST','localhost'); //Оставляем localhost, так как БД находится на одном сервере с php скриптами.
